b8c99d3bffedb06ce0b2e69bcec7292a41445b7e
hadibensaido <hadibensaido@yahoo.fr>
2016-07-11 17:26:38 +0200

    add first class

---
+++ CustomerSatisfaction/Models/Segment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSatisfaction.Models
+{
+    public class Segment
+    {
+
+        int idSegment { get; set; }
+        string libelleSegment { get; set; }
+    }
+}
---
+++ CustomerSatisfaction/Models/SousSegment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSatisfaction.Models
+{
+    public class SousSegment :Segment
+    {
+        int idSSegment { get; set; }
+        string libelleSSegment { get; set; }
+    }
+}
