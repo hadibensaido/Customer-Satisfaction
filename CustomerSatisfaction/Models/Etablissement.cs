64ef68dd905841e00c9cbeb18861645cc80dd150
StevyDubo <stevy.dubo@gmail.com>
2016-07-11 22:39:54 +0200

    Ajout de la classe établissement

---
+++ CustomerSatisfaction/Models/Etablissement.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSatisfaction.Models
+{
+    public class Etablissement
+    {
+        #region Champs Etablissement
+        public int idEtablissement;
+        public string nameEtablissement {get; private set;}
+        private ICollection<Client> clients;
+        //private ICollection<IUser> users;
+        #endregion
+    }
+}
