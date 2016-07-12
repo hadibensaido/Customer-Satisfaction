83d14e9b9ebaf5a0ecfdd5101bb3a3bc7a035043
Stévy <Stvy@ASTON>
2016-07-12 15:25:05 +0200

    modification de la classe établissement

--- MetierSatisfaction/Etablissement.cs
+++ MetierSatisfaction/Etablissement.cs
@@ -9,5 +9,8 @@ namespace CustomerSatisfaction.Models
     {
         public int idEtablissement { get; set; }
         public string libelleEtablissement { get; set; }
+        public virtual ICollection<Client> clients { get; private set; }
+        public virtual ICollection<Utilisateur> utilisateurs { get; private set; }
+        public virtual ICollection<Audit> audit { get; private set; }
     }
 }
