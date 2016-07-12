537d4c54d87c17ed279e1e4df69edf0c4d8da8c7
hadibensaido <hadibensaido@yahoo.fr>
2016-07-12 10:22:36 +0200

    update class diagram

--- CustomerSatisfaction/Models/Question.cs
+++ CustomerSatisfaction/Models/Question.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace CustomerSatisfaction.Models
 {
-    public class Question:Prestations
+    public class Question
     {
         public int idQuestion { get; set; }
         public string libelleQuestion { get; set; }
